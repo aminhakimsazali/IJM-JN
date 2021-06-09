0b92b5af4d95be29a3d20b1bc6eb18fc0ec020b1
aminhakimsazali <aminhakimsazali@gmail.com>
2021-06-09 11:05:54 +0800

    second edit:

--- ip_cam.py
+++ ip_cam.py
@@ -55,9 +55,18 @@ def encodeFrame():
         yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
             bytearray(encoded_image) + b'\r\n')
 
-@app.route("/")
+@app.route("/video_feed")
 def streamFrames():
     return Response(encodeFrame(), mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+
+# @app.route('/video_feed')
+# def video_feed():
+#     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')    
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 # check to see if this is the main thread of execution
 if __name__ == '__main__':
